d1f8c401a39d07d4ec7f0a4bc0c50c942dbc471d
Emma <emmashir230307@gmail.com>
2025-08-27 13:53:27 +0300

    working

---
+++ menu.py
@@ -0,0 +1,11 @@
+import consts
+
+
+class menu:
+    def __init__(self,id,password):
+        if id in consts.USERS_DICT.keys():
+            pass
+        else:
+            pass
+    def signup(self):
+        pass
